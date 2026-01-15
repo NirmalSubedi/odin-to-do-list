1efc0b982970a3b882367ffc666cd1e789d9fc86
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-01-14 19:14:10 -0800

    Style todo dialog

--- src/display.js
+++ src/display.js
@@ -1,5 +1,11 @@
-const renderPage=()=>{
-    
+const renderPage = () => {
+
 }
 
-export {renderPage};
+const renderTodoDialog = () => {
+    const dialog = document.querySelector('.todo-dialog');
+    dialog.showModal();
+}
+
+renderTodoDialog();
+export { renderPage };
