51e7aa20d013ba625e8af47c5eef090ff43495ee
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-01-24 13:49:45 -0800

    Add function to unregister action

--- src/display/actions.js
+++ src/display/actions.js
@@ -2,11 +2,11 @@ const actions = [];
 
 function registerAction(action, ...elements) {
     if (typeof action !== 'function') throw new TypeError('Action must be a function.');
-    if (elements.length === 0) throw new Error('Need at least one element to register.')
+    if (elements.length === 0) throw new Error('Need at least one element to register.');
     for (const element of elements) {
         if (typeof element !== 'object') throw new TypeError('Element must be an object.');
         if (!(element instanceof HTMLElement)) throw new Error('Element must be an instance of HTMLElement.');
-        if (getAction(element) !== undefined) throw new Error('Element is already registered.')
+        if (getAction(element) !== undefined) throw new Error('Element is already registered.');
     };
 
     actions.push({
@@ -21,9 +21,15 @@ function getAction(element) {
     );
 };
 
-function removeRegister(){
+function unregisterAction(element) {
+    if (typeof element !== 'object') throw new TypeError('Element must be an object.');
+    if (!(element instanceof HTMLElement)) throw new Error('Element must be an instance of HTMLElement.');
+    if (getAction(element) === undefined) throw new Error('Element is not registered.');
 
+    const action = getAction(element);
+    const actionIndex = actions.indexOf(action);
+    actions.splice(actionIndex,1);
 };
 
 
-export { getAction, registerAction };
+export { getAction, registerAction, unregisterAction };
